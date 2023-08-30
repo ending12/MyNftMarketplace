17e8f4a3cf011a6bfa9f10c9fe1a903e5e6c7408
kuk <kuk90129@gmail.com>
2023-08-30 16:38:45 +0800

    MyNftMarketplacev0.0011 增加readme

--- MyMarketplaceBacked/helper-hardhat-config.js
+++ MyMarketplaceBacked/helper-hardhat-config.js
@@ -6,7 +6,7 @@ const networkConfig = {
     5: {
         name: "goerli",
         subscriptionId: "588",
-        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
+        gasLane: "#", // 30 gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: "100000000000000000", // 0.1 ETH
         callbackGasLimit: "500000", // 500,000 gas
